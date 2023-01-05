1ecd6ba5b2894e2f004be464f23f8ce9c17020e7
thuylawrence <thuylawrence@byui.edu>
2023-01-05 14:54:27 +0800

    changes added

---
+++ scripts/getDates.js
@@ -0,0 +1,12 @@
+let text = document.lastModified;
+const currentYear = new Date().getFullYear();
+const paragraph = `
+      <p>
+        @${new Date().getFullYear()} Thuy Duong Thi Lawrence - USA
+      </p>
+    `;
+document.getElementById("copyright").innerHTML = paragraph
+
+var date = document.lastModified;
+
+document.getElementById("lastModified").innerHTML = "Last Modification: " + date;
