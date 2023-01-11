c02734506888c121ae3228abf5c8559c4f634f24
thuylawrence <thuylawrence@byui.edu>
2023-01-12 00:22:48 +0800

    changes added

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -10,3 +10,12 @@ document.getElementById("copyright").innerHTML = paragraph
 var date = document.lastModified;
 
 document.getElementById("lastModified").innerHTML = "Last Modification: " + date;
+
+const menubutton = document.querySelector("#menu-button");
+const menuitems = document.querySelectorAll(".menu-item");
+// menuitems will be a Node List of the list items
+
+menubutton.addEventListener("click", () => {
+	menuitems.forEach((item) => item.classList.toggle("open"));
+	menubutton.classList.toggle("close");
+});
