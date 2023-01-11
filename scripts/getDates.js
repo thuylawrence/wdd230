64d21da27af9ecf77f664eaffb8371c7fedb0836
thuylawrence <thuylawrence@byui.edu>
2023-01-12 01:32:49 +0800

    changes added

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,17 +5,4 @@ const paragraph = `
         @${new Date().getFullYear()} Thuy Duong Thi Lawrence - USA
       </p>
     `;
-document.getElementById("copyright").innerHTML = paragraph
 
-var date = document.lastModified;
-
-document.getElementById("lastModified").innerHTML = "Last Modification: " + date;
-
-const menubutton = document.querySelector("#menu-button");
-const menuitems = document.querySelectorAll(".menu-item");
-// menuitems will be a Node List of the list items
-
-menubutton.addEventListener("click", () => {
-	menuitems.forEach((item) => item.classList.toggle("open"));
-	menubutton.classList.toggle("close");
-});
