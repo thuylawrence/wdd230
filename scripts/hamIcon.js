f0d6a48e9c2dd2e095c5b70e77fbd2a281e60133
thuylawrence <thuylawrence@byui.edu>
2023-01-15 00:49:56 +0800

    changes added

--- scripts/hamIcon.js
+++ scripts/hamIcon.js
@@ -4,15 +4,14 @@ var date = document.lastModified;
 
 document.getElementById("lastModified").innerHTML = "Last Modification: " + date;
 
-const menubutton = document.querySelector("#hamIcon");
-const menuitems = document.querySelectorAll(".nav-item");
+// const menubutton = document.querySelector("#hamIcon");
+// const menuitems = document.querySelectorAll(".nav-item");
  
-// menuitems will be a Node List of the list items
+// // menuitems will be a Node List of the list items
 
- menubutton.addEventListener("click", () => {
-	 menuitems.forEach((item) => item.classList.toggle("open"));
-	menubutton.classList.toggle("close");
- });
-// document.getElementById("hamIcon").addEventListener("click", function (){
-// 	document.getElementById("mainnav").classList.toggle("responsive");
-// }, false);
+//  menubutton.addEventListener("click", () => {
+// 	 menuitems.forEach((item) => item.classList.toggle("open"));
+// 	menubutton.classList.toggle("close");
+//  });
+document.getElementById("hamIcon").addEventListener("click", function (){
+	document.getElementById("mainnav").classList.toggle("responsive"); }, false);
