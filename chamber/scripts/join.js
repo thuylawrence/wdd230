69768fc859a078c3bec3416a0c3305e737dee327
thuylawrence <thuylawrence@byui.edu>
2023-02-04 11:38:42 -0600

    more changes

---
+++ chamber/scripts/join.js
@@ -0,0 +1,13 @@
+
+// document.getElementById("range").addEventListener("change", function(){
+//     popup("the range was changed to value " + this.value);
+//   });
+//   document.getElementById("form").addEventListener("submit", function(e){
+//     e.preventDefault();
+  
+//     if(document.getElementById("user1").value != document.getElementById("user2").value){
+//         alert("the usernames do not match");
+//         return;
+//     }
+  
+   
--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,8 +1,11 @@
-let text = document.lastModified;
-const currentYear = new Date().getFullYear();
-const paragraph = `
+
+const paragraph2 = `
       <p>
-        @${new Date().getFullYear()} Thuy Duong Thi Lawrence - USA
+        @${new Date().getFullYear()} Thuy Duong Thi Lawrence - WDD230 Project
       </p>
     `;
-    
+document.getElementById("copyright").innerHTML = paragraph2
+
+const date2 = document.lastModified;
+
+document.getElementById("lastModified").innerHTML = "Last Modification: " + date2;
